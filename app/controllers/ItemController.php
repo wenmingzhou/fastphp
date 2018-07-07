2130aa3f165746c1ad2d71721791efa87c591e67
zhouwenming <zhouwenming@house365.com>
2018-07-07 13:10:31 +0800

    设置默认文件打开地址

---
+++ app/controllers/ItemController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+
+
+class ItemController
+{
+    // 首页方法，测试框架自定义DB查询
+    public function index()
+    {
+        echo "welcome to php ";
+    }
+
+
+}
--- fastphp/fastphp.php
+++ fastphp/fastphp.php
@@ -1,6 +1,5 @@
 <?php
 namespace fastphp;
-
 // 框架根目录
 defined('CORE_PATH') or define('CORE_PATH', __DIR__);
 
@@ -11,12 +10,10 @@ class Fastphp
 {
     // 配置内容
     protected $config = array();
-
     public function __construct($config)
     {
         $this->config = $config;
     }
-
     // 运行程序
     public function run()
     {
@@ -31,31 +28,25 @@ class Fastphp
     // 路由处理
     public function route()
     {
-
         $controllerName = $this->config['defaultController'];
         $actionName = $this->config['defaultAction'];
         $param = array();
-
         $url = $_SERVER['REQUEST_URI'];
         // 清除?之后的内容
         $position = strpos($url, '?');
         $url = $position === false ? $url : substr($url, 0, $position);
         // 删除前后的“/”
         $url = trim($url, '/');
-
         if ($url) {
             // 使用“/”分割字符串，并保存在数组中
             $urlArray = explode('/', $url);
             // 删除空的数组元素
             $urlArray = array_filter($urlArray);
-
             // 获取控制器名
             $controllerName = ucfirst($urlArray[0]);
-
             // 获取动作名
             array_shift($urlArray);
             $actionName = $urlArray ? $urlArray[0] : $actionName;
-
             // 获取URL参数
             array_shift($urlArray);
             $param = $urlArray ? $urlArray : array();
@@ -63,19 +54,16 @@ class Fastphp
 
         // 判断控制器和操作是否存在
         $controller = 'app\\controllers\\'. $controllerName . 'Controller';
-
         if (!class_exists($controller)) {
             exit($controller . '控制器不存在');
         }
         if (!method_exists($controller, $actionName)) {
             exit($actionName . '方法不存在');
         }
-
         // 如果控制器和操作名存在，则实例化控制器，因为控制器对象里面
         // 还会用到控制器名和操作名，所以实例化的时候把他们俩的名称也
         // 传进去。结合Controller基类一起看
         $dispatch = new $controller($controllerName, $actionName);
-
         // $dispatch保存控制器实例化后的对象，我们就可以调用它的方法，
         // 也可以像方法中传入参数，以下等同于：$dispatch->$actionName($param)
         call_user_func_array(array($dispatch, $actionName), $param);
@@ -146,7 +134,6 @@ class Fastphp
     public function loadClass($className)
     {
         $classMap = $this->classMap();
-
         if (isset($classMap[$className])) {
             // 包含内核文件
             $file = $classMap[$className];
