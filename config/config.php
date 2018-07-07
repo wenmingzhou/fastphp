81a8e2e07b35e8ded8dd3221dab7762497018f97
zhouwenming <zhouwenming@house365.com>
2018-07-07 12:17:51 +0800

    初始化文件配置

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/7
+ * Time: 12:13
+ */
+
+// 数据库配置
+$config['db']['host'] = 'localhost';
+$config['db']['username'] = 'root';
+$config['db']['password'] = '123456';
+$config['db']['dbname'] = 'project';
+
+// 默认控制器和操作名
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
